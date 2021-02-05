60902d0ff4ab7c3bc4c1778e22a2d64194048803
Ashim Chalise <a_chali2@uncg.edu>
2021-02-05 12:32:06 -0500

    updated login again

---
+++ code/src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  template: `
+    
+      <h1> login works yay! </h1>
+    
+  `,
+  styles: [
+  ]
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
