c8bec40315ae03291329f7a6c6453f16f04e069b
lthammon <47529376+lthammon@users.noreply.github.com>
2021-02-19 13:20:51 -0500

    app components

--- code/src/app/app.module.ts
+++ code/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountsetupComponent } from './accountsetup/accountsetup.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { IncomeComponent } from './income/income.component';
+import { IncomeSourcesComponent } from './income-sources/income-sources.component';
+import { HousingComponent } from './housing/housing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    AccountsetupComponent,
+    ExpensesComponent,
+    IncomeComponent,
+    IncomeSourcesComponent,
+    HousingComponent
   ],
   imports: [
     BrowserModule,
