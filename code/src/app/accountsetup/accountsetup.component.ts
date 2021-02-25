a21e2c09f764764e57a7d66210c2d828829afd3f
Jonathan E. Bacon <38441580+GraveLime@users.noreply.github.com>
2021-02-25 14:21:54 -0500

    hooked functionality for creating users

--- code/src/app/accountsetup/accountsetup.component.ts
+++ code/src/app/accountsetup/accountsetup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../services/database.service';
+import { User } from '../models/user.model'
 
 @Component({
   selector: 'app-accountsetup',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountsetupComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private dbService: DatabaseService) { }
+  user: User = {
+    fName: '',
+    lName: '',
+    email: '',
+    password: '',
+  };
+  created = false;
   ngOnInit(): void {
+  }
+
+  addUser(): void {
+    this.user.fName = (document.getElementById("fName") as HTMLInputElement).value
+    this.user.lName = (document.getElementById("lName") as HTMLInputElement).value
+    this.user.email = (document.getElementById("email") as HTMLInputElement).value
+    this.user.password = (document.getElementById("password") as HTMLInputElement).value
+    
+    this.dbService.create(this.user).subscribe(
+      response => {
+        console.log(response)
+        this.created=true;
+      },
+      error => {
+        console.log(error)
+      });
   }
 
 }
