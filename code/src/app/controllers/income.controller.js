139f3007241379965d75b784df40b386d8cb7168
JJStadl2 <69908346+JJStadl2@users.noreply.github.com>
2021-03-16 16:01:47 -0400

    Create income.controller.js
    Created income controller with need functions for communication/usage of DB

---
+++ code/src/app/controllers/income.controller.js
@@ -0,0 +1,52 @@
+const { nextTick } = require('process');
+const Income = require('../models/income.model');
+
+// Create and Save a new User Object into the DB.
+exports.create = (req, res) => {
+    const _income = new Income({
+        SRCNAME: req.params.SRCNAME,
+        AMOUNT: req.params.AMOUNT,
+        PAYCYCLE: req.params.PAYCYCLE,
+        USERID: req.params.USERID
+      });
+
+    Income.create(_income,(err, data) => {
+        res.send(data);
+        });
+    };
+
+
+exports.getAll = (req, res) => {
+    Income.getAll((err, data) => {
+    if (err)
+        res.status(500).send({
+        message:
+        err.message || "Some error occurred while retrieving users."
+        });
+        else res.send(data);
+    });
+};
+exports.updateById = (req, res) =>{
+    Income.updateById(req.params.INCOMEID, req.params.USERID,(err,data) =>{
+        if(err){
+            res.send("update failed: No Income source by with that ID")
+        }
+        else{
+            res.send("Updated")
+        }
+        res.send(data);
+    });
+};
+exports.remove = (req, res) =>{
+    Income.remove(req.params.INCOMEID, req.params.USERID,(err,data)=>{
+        if(err){
+            res.send("Remove failed: No Income source by with that ID")
+        }
+        else{
+            res.send("Removed")
+        }
+        res.send(data);
+    });
+    //add remove all? is remove all missing * in sql code in app? 
+}
+
