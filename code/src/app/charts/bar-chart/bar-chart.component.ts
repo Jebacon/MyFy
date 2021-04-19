f2438b4e346bf062dad86b0ca03bc919ac3534fe
JJStadl2 <69908346+JJStadl2@users.noreply.github.com>
2021-04-19 11:09:34 -0400

    Update bar-chart.component.ts
    added logic for analytics. am testing now

--- code/src/app/charts/bar-chart/bar-chart.component.ts
+++ code/src/app/charts/bar-chart/bar-chart.component.ts
@@ -3,37 +3,85 @@ import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { Label } from 'ng2-charts';
 import { DatabaseService } from '../../services/database.service'
+
 @Component({
   selector: 'app-chart',
   templateUrl: './bar-chart.component.html',
   styleUrls: [ './bar-chart.component.css' ]
 })
 export class BarChartComponent  {
-  
-  barChartOptions: ChartOptions = {
+  public barChartOptions: ChartOptions = {
     responsive: true,
   };
-  barChartLabels: Label[] = ['Housing', 'Investments/Savings', 'Debt', 'Expenses', 'Discretionary'];
-  barChartType: ChartType = 'bar';
-  barChartLegend = true;
-  barChartPlugins = [];
+  public barChartLabels: Label[] = ['Housing', 'Investments/Savings', 'Debt', 'Expenses'];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+  public barChartPlugins = [];
 
-    barChartData: ChartDataSets[] = [
+    public barChartData: ChartDataSets[] = [
       { data: [100, 100, 100, 100, 100, 100, 100], label: 'Target'},
       { data: [(28), 48, 40, 19, 86, 27, 90], label: 'Actual'}
     ];
 
-  constructor(private dbService: DatabaseService) {
-    
-   
-   }
- 
+  constructor(private dbService: DatabaseService) { }
 
-  ngOnInit(actual:any[], target:any[]) {
-    // target values
-    this.barChartData[0]['data'] = target;
-    this.barChartData[1]['data'] = actual;
+  ngOnInit() {
+    var sessionId = window.sessionStorage.getItem("ID");
+    var total_income = 0;
+    var total_debts = 0;
+    var total_housing = 0;
+    var total_invest_save = 0;
+    var total_expense = 0;
+    // for others we need either all caps or camel case or other- look in dashboard compents at 200 at change table (see cases)
+    this.dbService.post("getUserIncome", {"USERID": sessionId}).subscribe(data =>{
+      for (var element in data){
+        var stringify = JSON.parse(JSON.stringify(data[element]))
+        var wage = stringify["AMOUNT"]
+        total_income += wage;
+      }
+    })
+    this.dbService.post("getUserDebts", {"userId": sessionId}).subscribe(data =>{
+      for (var element in data){
+        var stringify = JSON.parse(JSON.stringify(data[element]))
+        var balance = stringify["BALANCE"]
+        total_debts += balance;
+      }
+    })
+    this.dbService.post("getUserExpenses", {"userId": sessionId}).subscribe(data =>{
+      for (var element in data){
+        var stringify = JSON.parse(JSON.stringify(data[element]))
+        var cost = stringify["COSTS"]
+        total_expense += cost;
+      }
+    })
+    this.dbService.post("getUserHousing", {"USERID": sessionId}).subscribe(data =>{
+      for (var element in data){
+        var stringify = JSON.parse(JSON.stringify(data[element]))
+        var cost = stringify["COSTS"]
+        total_housing += cost;
+      }
+    })
+    this.dbService.post("getUserInvestments", {"userId": sessionId}).subscribe(data =>{
+      for (var element in data){
+        var stringify = JSON.parse(JSON.stringify(data[element]))
+        var invest_amnt  = stringify["INVEST_AMNT$"]
+        total_invest_save += invest_amnt;
+      }
+    })
+    var housingT = total_income* .3;
+    var investT = total_income*.1;
+    var debtT = total_income* .10;
+    var expenseT = total_income* .5;
+    
+    //['Housing', 'Investments/Savings', 'Debt', 'Expenses', 'Discretionary']
+    //var targets by percents- will store in the list below
+    this.barChartData[0]['data'] = [housingT, investT, debtT, expenseT];
+    this.barChartData[1]['data'] = [total_housing, total_invest_save, total_debts, total_expense];
+
+    
+    
 
   }
 }
+
 
