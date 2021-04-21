9f75b97a15f1914eac8e81181af3aec14c8cabb8
Jonathan E. Bacon <38441580+GraveLime@users.noreply.github.com>
2021-04-21 18:15:40 -0400

    dashboard fixes for use with chrome browser

--- code/src/app/dashboard/dashboard.component.ts
+++ code/src/app/dashboard/dashboard.component.ts
@@ -123,9 +123,14 @@ export class DashboardComponent implements OnInit {
       }
     }
     //manages switching between input types for asset creation
-    dataType(data: string) : void{
-      this.Current_input = data
-      if(data == "Debt"){
+    dataType() : void{
+      var type = (document.getElementById("Type") as HTMLSelectElement).value
+      if(this.Current_input == type){
+        return
+      }
+      this.Current_input = type
+     
+      if(this.Current_input == "Debt"){
         (document.getElementById("rateDiv") as HTMLElement).style.display = "block";
       } else {
         (document.getElementById("rateDiv") as HTMLElement).style.display = "none";
@@ -336,74 +341,4 @@ export class DashboardComponent implements OnInit {
       }
     }
 
-    go_analytics(): void{
-      console.log("fuck")
-
-      var sessionId = window.sessionStorage.getItem("ID");
-
-      this.dbService.post("getUserIncome", {"USERID": sessionId}).subscribe(data =>{
-        var local_income = 0
-        for (var element in data){
-          var stringify = JSON.parse(JSON.stringify(data[element]))
-          var wage = stringify["AMOUNT"]
-          local_income += wage;
-        }
-        console.log('in barchart: Total Income: ', local_income);
-        window.sessionStorage.setItem("Income", ""+local_income)
-      })
-
-     this.dbService.post("getUserDebts", {"userId": sessionId}).subscribe(data =>{
-       var local_debts = 0
-       for (var element in data){
-         var stringify = JSON.parse(JSON.stringify(data[element]))
-         var balance = stringify["BALANCE"]
-         local_debts += balance;
-       }
-       window.sessionStorage.setItem("Debt", ""+local_debts)
-     })
-     this.dbService.post("getUserExpenses", {"userId": sessionId}).subscribe(data =>{
-       var local_expense = 0
-       for (var element in data){
-         var stringify = JSON.parse(JSON.stringify(data[element]))
-         var cost = stringify["COSTS"]
-         local_expense += cost;
-       }
-       window.sessionStorage.setItem("Expense", ""+local_expense)
-     })
-     this.dbService.post("getUserHousing", {"USERID": sessionId}).subscribe(data =>{
-       var local_housing = 0
-       for (var element in data){
-         var stringify = JSON.parse(JSON.stringify(data[element]))
-         var cost = stringify["COSTS"]
-         local_housing += cost;
-       }
-       window.sessionStorage.setItem("Housing", ""+local_housing)
-     })
-     this.dbService.post("getUserInvestments", {"userId": sessionId}).subscribe(data =>{
-       var local_invest_save = 0
-       for (var element in data){
-         var stringify = JSON.parse(JSON.stringify(data[element]))
-         var invest_amnt  = stringify["INVEST_AMNT$"]
-         local_invest_save += invest_amnt;
-       }
-       window.sessionStorage.setItem("Invest_save", ""+local_invest_save)
-     })
-     var total_income = 0
-     total_income = +window.sessionStorage.getItem("Income")!
-    //var housingT = total_income * .3;
-    //  var investT = total_income*.1;
-    //  var debtT = total_income* .10;
-    //  var expenseT = total_income* .5;
-
-     window.sessionStorage.setItem("housingT",""+(total_income * .3))
-     window.sessionStorage.setItem("investT",""+(total_income * .1))
-     window.sessionStorage.setItem("debtT",""+(total_income * .1))
-     window.sessionStorage.setItem("expenseT",""+(total_income * .5))
-     setTimeout(()=>
-     {
-      this.router.navigate(['analytics'])
-     },
-     5000);
-      
-    }
 }
